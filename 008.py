5ef59f87ac5fafe94f4085967dedaea00b6caacb
kanyarath <109723699+kanyarath@users.noreply.github.com>
2022-07-21 15:00:37 +0700

    เพิ่ม

--- 008.py
+++ 008.py
@@ -2,3 +2,6 @@ Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (
 Type "help", "copyright", "credits" or "license()" for more information.
 print("Hello dear friend")
 Hello dear friend
+
+print("by kanyarath")
+by kanyarath
